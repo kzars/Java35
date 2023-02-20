b138af9df8409f835edf45dcce80dffd9f77ff83
Kārlis Zars <zarskarlis@gmail.com>
2023-02-20 21:27:06 +0200

    Switch, NestedIf ,For loop

---
+++ src/basics/Loops.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("Hello world!");
+        }
+
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i = 1; i <= 10; i += 2){
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
--- src/homeworks/HW2.java
+++ src/homeworks/HW2.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW2 {
     public static void main(String[] args) {
 
@@ -8,6 +10,13 @@ public class HW2 {
         // Example with input 122.7 mp/h
         // Output --> 122.7 mp/h in km/h would be equal to 197.46651
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter speed in mp/h");
+        float speedMph = scanner.nextFloat();
+
+        float speedKmh = speedMph * 1.60934f;
+
+        System.out.println( speedMph + " mp/h in km/h would be equal to " + speedKmh);
 
 
     }
