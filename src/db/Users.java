73dcc9589058875c7ae6c22c36dcfc3ca50527ff
Kārlis Zars <zarskarlis@gmail.com>
2023-04-21 18:42:02 +0300

    Test

--- src/db/Users.java
+++ src/db/Users.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Users {
     public static void main(String[] args) {
-        //Test
 
         String dbURL = "jdbc:mysql://localhost:3306/java35";
         String username = "root";
